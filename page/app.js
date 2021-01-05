87cddd88e3b4005ed4491716e2c509590b450785
hachiko <mertuygur02@gmail.com>
2021-01-06 01:22:43 +0300

    question added :)

--- page/app.js
+++ page/app.js
@@ -8,3 +8,39 @@ const y = document.querySelector('.logo')
 function change() {
     y.classList.replace('logo', 'logotrue')
 }
+
+
+x1 = document.querySelector('#one');
+x2 = document.querySelector('#two');
+x3 = document.querySelector('#three');
+x4 = document.querySelector('#four');
+
+image = document.querySelector('.image');
+sect = document.querySelector('.quest');
+
+x1.addEventListener('click', () => {
+    sect.classList.add('wrong')
+    setTimeout(() => {
+        sect.classList.remove('wrong')
+    }, 1000)
+    x1.style.display = "none";
+})
+
+x2.addEventListener('click', () => {
+    image.classList.replace('image', 'imagetrue');
+    sect.classList.add('correct')
+})
+x3.addEventListener('click', () => {
+    sect.classList.add('wrong')
+    setTimeout(() => {
+        sect.classList.remove('wrong')
+    }, 1000)
+    x3.style.display = "none";
+})
+x4.addEventListener('click', () => {
+    sect.classList.add('wrong')
+    setTimeout(() => {
+        sect.classList.remove('wrong')
+    }, 1000)
+    x4.style.display = "none";
+})
