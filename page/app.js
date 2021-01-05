e025aa5079737838bd8ca932bd7a5bcc95404b10
hachiko <mertuygur02@gmail.com>
2021-01-05 19:24:23 +0300

    backgrounds, notebook data adding, some other things

---
+++ page/app.js
@@ -0,0 +1,10 @@
+const x = document.querySelector('.centered-image')
+function showup() {
+    x.classList.replace('centered-image', 'dstrue')
+}
+
+const y = document.querySelector('.logo')
+
+function change() {
+    y.classList.replace('logo', 'logotrue')
+}
